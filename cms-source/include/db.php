17948482178d8668e077b8ffc5c3bcf75e632184
Benjamin Marie Kamano <kamanobenjamin@gmail.com>
2020-11-22 16:30:22 +0000

    source

---
+++ cms-source/include/db.php
@@ -0,0 +1,12 @@
+<?php 
+  $connect= mysqli_connect('localhost','root','','cms');
+  if (!$connect) {
+  	   die('<h1>Erreur de connection à la Base de données</h1>');
+  }
+  else{
+
+  	echo'okkkk';
+  }
+
+
+ ?>
